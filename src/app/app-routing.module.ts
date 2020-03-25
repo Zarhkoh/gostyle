ea2cdf113d7079622c16f7f4ea3804cea16ca5b6
zarhkoh <zarhkoh@hotmail.fr>
2020-03-25 17:40:40 +0100

    fix tabs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,25 +5,39 @@ const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
   {
-    path: 'account',
-    loadChildren: () => import('./account/account.module').then( m => m.AccountPageModule)
+    path: 'tabs',
+    loadChildren: () => import('./tabs/tabs.module').then( m => m.TabsPageModule)
   },
   {
-    path: 'contact',
-    loadChildren: () => import('./contact/contact.module').then( m => m.ContactPageModule)
+    path: 'tabs/account',
+    children: [
+      {
+        path: '',
+        loadChildren: () =>
+            import('./account/account.module').then(m => m.AccountPageModule)
+      }
+    ]
   },
   {
-    path: 'scanner',
-    loadChildren: () => import('./scanner/scanner.module').then( m => m.ScannerPageModule)
+    path: 'tabs/scanner',
+    children: [
+      {
+        path: '',
+        loadChildren: () =>
+            import('./scanner/scanner.module').then(m => m.ScannerPageModule)
+      }
+    ]
   },
   {
-    path: 'coupon-list',
-    loadChildren: () => import('./coupon-list/coupon-list.module').then( m => m.CouponListPageModule)
-  },
-  {
-    path: 'login',
-    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
-  },
+    path: 'tabs/coupon-list',
+    children: [
+      {
+        path: '',
+        loadChildren: () =>
+            import('./coupon-list/coupon-list.module').then(m => m.CouponListPageModule)
+      }
+    ]
+  }
 ];
 
 @NgModule({
