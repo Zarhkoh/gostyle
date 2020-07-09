fd1fb8f3b7270c633966b9d971cac9cbf0ad0bea
Justine Moreau <p.justine.h@gmail.com>
2020-07-09 15:25:43 +0200

    commencement des test

--- src/app/account/account.page.spec.ts
+++ src/app/account/account.page.spec.ts
@@ -1,5 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
+import { By } from '@angular/platform-browser';
 
 import { RouterTestingModule } from '@angular/router/testing';
 import { AccountPage } from './account.page';
@@ -22,4 +23,14 @@ describe('AccountPage', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  // it(' Le info du compte sont corrects  ', () => {
+  //   const nom = fixture.debugElement.query(By.css('.nom')).nativeElement;
+  //   const prenom = fixture.debugElement.query(By.css('.prenom')).nativeElement;
+  //   const email = fixture.debugElement.query(By.css('.email')).nativeElement;
+  //   expect(nom.innerHTML).not.toEqual("Nom:moreau");
+  //   expect(prenom.innerHTML).not.toEqual("Prénom:justine");
+  //   expect(email.innerHTML).not.toEqual("Adresse e-mail:justine.moreau@epsi.fr");
+  //   });
+
 });
--- src/app/coupon-list/coupon-list.page.spec.ts
+++ src/app/coupon-list/coupon-list.page.spec.ts
@@ -3,6 +3,9 @@ import { IonicModule } from '@ionic/angular';
 
 import { RouterTestingModule } from '@angular/router/testing';
 import { CouponListPage } from './coupon-list.page';
+import { HttpClient, HttpHandler } from '@angular/common/http';
+import { SQLite } from '@ionic-native/sqlite/ngx';
+
 
 describe('CouponListPage', () => {
   let component: CouponListPage;
@@ -10,6 +13,7 @@ describe('CouponListPage', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      providers:[HttpClient, HttpHandler,SQLite],
       declarations: [ CouponListPage ],
       imports: [IonicModule.forRoot(),RouterTestingModule]
     }).compileComponents();
